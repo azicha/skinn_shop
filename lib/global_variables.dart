e4c44211df0d5d859bbce27fa7a5a04eab013bee
azicha <askakauchi@gmail.com>
2024-01-23 22:27:23 +0300

    Add a list of products

---
+++ lib/global_variables.dart
@@ -0,0 +1,62 @@
+final products = [
+  {
+    'id': 0,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 1,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 2,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 3,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 4,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 5,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 6,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 7,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 8,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 9,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 10,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+  {
+    'id': 11,
+    'productName': 'aska',
+    'productImage': 'aska',
+  },
+];
