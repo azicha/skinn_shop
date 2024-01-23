8c80f41c75ae418b08775638fe5ffa47e4df2139
azicha <askakauchi@gmail.com>
2024-01-23 22:29:28 +0300

    Code refactoring

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:skinn_shop/global_variables.dart';
+import 'package:skinn_shop/product_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(
+          products.length,
+          (index) {
+            final product = products[index];
+            return ProductCard(
+              productName: product['productName'] as String,
+              productImage: product['productImage'] as String,
+            );
+          },
+        ),
+      ),
+    );
   }
 }
