af95ed1b14d703c32d20f0813b1ca800d6bbab6b
azicha <askakauchi@gmail.com>
2024-01-24 23:01:05 +0300

    Code refactoring

--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -2,61 +2,128 @@ final products = [
   {
     'id': 0,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=skc69a2uf',
   },
   {
     'id': 1,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=skc69a2uf',
   },
   {
     'id': 2,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=skc69a2uf',
   },
   {
     'id': 3,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=skc69a2uf',
   },
   {
     'id': 4,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=skc69a2uf',
   },
   {
     'id': 5,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=skc69a2uf',
   },
   {
     'id': 6,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=skc69a2uf',
   },
   {
     'id': 7,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=skc69a2uf',
   },
   {
     'id': 8,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=skc69a2uf',
   },
   {
     'id': 9,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=d2k3zbw89',
   },
   {
     'id': 10,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=9dquh483j',
   },
   {
     'id': 11,
     'productName': 'aska',
-    'productImage': 'aska',
+    'productImage': 'https://files.fm/thumb_show.php?i=pvhq84npw',
   },
 ];
+// final products = [
+//     {
+//         id: 1,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=d2k3zbw89",
+//         itemDescription: "CeraVe Face Wash Acne Treatment | Clogged Pore Control"
+//     },
+//     {
+//         id: 2,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=9dquh483j",
+//         itemDescription: "Garnier Vitamin C* Moisturizing Brightening Day Cream with Super Citrus"
+//     },
+//     {
+//         id: 3,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=pvhq84npw",
+//         itemDescription: "Garnier Skinactive Clearly Brighter Dark Spot Corrector with Vitamin C"
+//     },
+//     {
+//         id: 4,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=yce9x8sed",
+//         itemDescription: "Hydrating Face Moisturizer with Avocado Sunflower Seed Oils, Dry to Normal Skin"
+//     },
+//     {
+//         id: 5,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=z4e2xx8zn",
+//         itemDescription: "AZURE Collagen & Vitamin C Anti Aging Eye Serum - Toning & De-puffing Serum"
+//     },
+//     {
+//         id: 6,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=nexrz49e2",
+//         itemDescription: "Facial Cleansing Brush Face Scrubber: Electric Exfoliating Spin Cleanser "
+//     },
+//     {
+//         id: 7,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=hk3pe8frz",
+//         itemDescription: "Dr Rashel Vitamin C Face Serum And Vitamin C Face Cream Variety Pack "
+//     },
+//     {
+//         id: 8,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=7k9j3az5w",
+//         itemDescription: "Garnier SkinActive Face Wash with Aloe Juice, For Dry Skin"
+//     },
+//     {
+//         id: 9,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=z29ay36se",
+//         itemDescription: "CeraVe Foaming Facial and Hydrating Cleanser, 24 Fluid Ounce by CeraVe"
+//     },
+//     {
+//         id: 10,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=th48qkszj",
+//         itemDescription: "PopSockets Lip Balm Grip with Expanding Kickstand, PopLips"
+//     },
+//     {
+//         id: 11,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=5kwdeyqcx",
+//         itemDescription: "Generic Glossy Lips Lip Glosses in Crystal Clear or Shimmering Glitter",
+//     },
+//     {
+//         id: 12,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=f2d7qja98",
+//         itemDescription: "CeraVe Face Sunscreen with Zinc Oxide & Titanium Oxide for Sensitive Skin",
+//     },
+//     {
+//         id: 13,
+//         itemImageLink: "https://files.fm/thumb_show.php?i=skc69a2uf",
+//         itemDescription: "Garnier SkinActive 3-in-1 Face Moisturizer with Green Tea, Oily Skin, 6.75 fl. oz.",
+//     },
+// ]
--- lib/product_card.dart
+++ lib/product_card.dart
@@ -20,15 +20,24 @@ class ProductCard extends StatelessWidget {
               width: double.infinity,
               height: 200,
               decoration: const BoxDecoration(
-                color: Colors.red,
+                color: Color.fromARGB(60, 144, 141, 141),
                 borderRadius: BorderRadius.all(
                   Radius.circular(5),
                 ),
               ),
               child: Column(
                 children: [
+                  const SizedBox(
+                    height: 5,
+                  ),
                   Text(productName),
-                  Text(productImage),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Image.network(
+                    productImage,
+                    height: 150,
+                  ),
                 ],
               ),
             ),
