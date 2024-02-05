be26f3171a240589c4cb49f69444829adc2bf031
azicha <askakauchi@gmail.com>
2024-02-05 23:07:57 +0300

    Code refactoring

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,25 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:skinn_shop/global_variables.dart';
 import 'package:skinn_shop/product_card.dart';
+import 'package:skinn_shop/product_description.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(225, 225, 225, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(50),
+      ),
+    );
     return Scaffold(
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(
-          products.length,
-          (index) {
-            final product = products[index];
-            return ProductCard(
-              productName: product['productName'] as String,
-              productImage: product['productImage'] as String,
-            );
-          },
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text('Skinn\n & Beauty',
+                      style: Theme.of(context).textTheme.titleLarge),
+                ),
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                children: List.generate(
+                  products.length,
+                  (index) {
+                    final product = products[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: ((context) {
+                          return ProductDescription(product: product);
+                        })));
+                      },
+                      child: ProductCard(
+                        productName: product['productName'] as String,
+                        productImage: product['productImage'] as String,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_basket),
+            label: '',
+          ),
+        ],
       ),
     );
   }
--- lib/product_card.dart
+++ lib/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skinn_shop/product_description.dart';
 
 class ProductCard extends StatelessWidget {
   final String productName;
@@ -12,38 +13,37 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 200,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(60, 144, 141, 141),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(productName),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Image.network(
-                    productImage,
-                    height: 150,
-                  ),
-                ],
+        body: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 200,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 240, 238, 238),
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
               ),
             ),
-          ],
-        ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(productName),
+                const SizedBox(
+                  height: 10,
+                ),
+                Image.network(
+                  productImage,
+                  height: 150,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
